cd967b0481bb8985e111795116339c009f371b2e
gsch57 <gaetan.schmit@ekonoo.com>
2024-02-05 15:49:47 +0100

    draw a simple circle !

--- main.cpp
+++ main.cpp
@@ -5,6 +5,46 @@
 #define WIDTH 800  // Screen width
 #define HEIGHT 600 // Screen height
 
+void DrawCircle(SDL_Renderer *renderer, int32_t centreX, int32_t centreY, int32_t radius)
+{
+    SDL_SetRenderDrawColor(renderer, 0, 0, 250, 255);
+
+    const int32_t diameter = (radius * 2);
+
+    int32_t x = (radius - 1);
+    int32_t y = 0;
+    int32_t tx = 1;
+    int32_t ty = 1;
+    int32_t error = (tx - diameter);
+
+    while (x >= y)
+    {
+        //  Each of the following renders an octant of the circle
+        SDL_RenderDrawPoint(renderer, centreX + x, centreY - y);
+        SDL_RenderDrawPoint(renderer, centreX + x, centreY + y);
+        SDL_RenderDrawPoint(renderer, centreX - x, centreY - y);
+        SDL_RenderDrawPoint(renderer, centreX - x, centreY + y);
+        SDL_RenderDrawPoint(renderer, centreX + y, centreY - x);
+        SDL_RenderDrawPoint(renderer, centreX + y, centreY + x);
+        SDL_RenderDrawPoint(renderer, centreX - y, centreY - x);
+        SDL_RenderDrawPoint(renderer, centreX - y, centreY + x);
+
+        if (error <= 0)
+        {
+            ++y;
+            error += ty;
+            ty += 2;
+        }
+
+        if (error > 0)
+        {
+            --x;
+            tx += 2;
+            error += (tx - diameter);
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_VIDEO);
@@ -43,7 +83,17 @@ int main(int argc, char *argv[])
         {
             if (SDL_QUIT == event.type)
                 finish = true;
+            if (SDL_KEYDOWN == event.type)
+            {
+                if (SDLK_ESCAPE == event.key.keysym.sym)
+                    finish = true;
+            }
         }
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // renderer, r, g, b, a
+        SDL_RenderClear(renderer);
+        DrawCircle(renderer, 100, 100, 40);
+        // Show the renderered frame
+        SDL_RenderPresent(renderer);
         SDL_Delay(100);
     }
 
